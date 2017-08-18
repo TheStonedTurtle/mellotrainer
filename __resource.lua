7848eec2becb2d9170ed39090623e07cee6b610e
WolfKnight98 <WolfKnight98@users.noreply.github.com>
2017-08-18 18:49:29 +0100

    Organised all of the files

--- __resource.lua
+++ __resource.lua
@@ -13,24 +13,21 @@ files {
 	"nui/Roboto-Black.ttf"
 }
 
-client_script 'utils.lua'
+client_script 'cl_utils.lua'                    -- Clientsided utilites.
+client_script 'cl_variables.lua'                -- Create all default variables. 
+client_script 'cl_general.lua'                  -- User Managment/Trainer Controls/Global Functions
+client_script 'cl_map.lua'                      -- Map Blips.
+client_script 'cl_player.lua'                   -- Player Toggles & Options
+client_script 'cl_settings.lua'                 -- General Settings (Player Blips etc)
+client_script 'cl_settings_voice.lua'           -- Handles all voice-chat related settings
+client_script 'cl_settings_notifications.lua'   -- Handles Player & Death Notifications
+client_script 'cl_player_skin.lua'              -- Player Skins & Props
+client_script 'cl_player_online.lua'            -- Other Player Options (Teleport/Spectate)
+client_script 'cl_vehicles.lua'                 -- Vehicle Spawning/Modifications
+client_script 'cl_weapons.lua'                  -- Weapon Spawning/Attachments
+client_script 'cl_admin.lua'                    -- Handles Admin Menu Access & Options
 
-client_scripts {
-	"variables.lua",      		  -- Create all default variables. 
-	"general.lua",        		  -- User Managment/Trainer Controls/Global Functions
-	"map.lua",			  		  -- Map Blips.
-	"player.lua",	      		  -- Player Toggles & Options
-	"settings.lua",	      		  -- General Settings (Player Blips etc)
-	"settings-voice.lua", 		  -- Handles all voice-chat related settings
-	"settings-notifications.lua", -- Handles Player & Death Notifications
-	"player-skin.lua",    		  -- Player Skins & Props
-	"player-online.lua",  		  -- Other Player Options (Teleport/Spectate)
-	"vehicles.lua",       		  -- Vehicle Spawning/Modifications
-	"weapons.lua",        		  -- Weapon Spawning/Attachments
-	"admin.lua" 		  		  -- Handles Admin Menu Access & Options
-}
-
-server_script 'sv_config.lua'
-server_script 'sv_main.lua'
-server_script 'sv_data_saving.lua'
-server_script 'sv_admin.lua'   -- Handles Cross-Player Admin Commands
+server_script 'sv_config.lua'       -- MelloTrainer config file
+server_script 'sv_main.lua'         -- Main serversided file 
+server_script 'sv_data_saving.lua'  -- Data saving system 
+server_script 'sv_admin.lua'        -- Handles Cross-Player Admin Commands
