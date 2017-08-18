39fc0024a7c868e5cd760c01a49f13f7c9831bb8
WolfKnight98 <WolfKnight98@users.noreply.github.com>
2017-08-18 23:22:07 +0100

    Changed client_script to client_scripts

--- __resource.lua
+++ __resource.lua
@@ -13,19 +13,21 @@ files {
 	"nui/Roboto-Black.ttf"
 }
 
-client_script 'cl_utils.lua'                    -- Clientsided utilites.
-client_script 'cl_variables.lua'                -- Create all default variables. 
-client_script 'cl_general.lua'                  -- User Managment/Trainer Controls/Global Functions
-client_script 'cl_map.lua'                      -- Map Blips.
-client_script 'cl_player.lua'                   -- Player Toggles & Options
-client_script 'cl_settings.lua'                 -- General Settings (Player Blips etc)
-client_script 'cl_settings_voice.lua'           -- Handles all voice-chat related settings
-client_script 'cl_settings_notifications.lua'   -- Handles Player & Death Notifications
-client_script 'cl_player_skin.lua'              -- Player Skins & Props
-client_script 'cl_player_online.lua'            -- Other Player Options (Teleport/Spectate)
-client_script 'cl_vehicles.lua'                 -- Vehicle Spawning/Modifications
-client_script 'cl_weapons.lua'                  -- Weapon Spawning/Attachments
-client_script 'cl_admin.lua'                    -- Handles Admin Menu Access & Options
+client_scripts { 
+	'cl_utils.lua',                    -- Clientsided utilites.
+	'cl_variables.lua',                -- Create all default variables. 
+	'cl_general.lua',                  -- User Managment/Trainer Controls/Global Functions
+	'cl_map.lua',                      -- Map Blips.
+	'cl_player.lua',                   -- Player Toggles & Options
+	'cl_settings.lua',                 -- General Settings (Player Blips etc)
+	'cl_settings_voice.lua',           -- Handles all voice-chat related settings
+	'cl_settings_notifications.lua',   -- Handles Player & Death Notifications
+	'cl_player_skin.lua',              -- Player Skins & Props
+	'cl_player_online.lua',            -- Other Player Options (Teleport/Spectate)
+	'cl_vehicles.lua',                 -- Vehicle Spawning/Modifications
+	'cl_weapons.lua',                  -- Weapon Spawning/Attachments
+	'cl_admin.lua'                    -- Handles Admin Menu Access & Options
+}
 
 server_script 'sv_config.lua'       -- MelloTrainer config file
 server_script 'sv_main.lua'         -- Main serversided file 
